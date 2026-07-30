920a7eaa6192f1746a5a9868dbb4723f2291f435
ybadr99 <yousefbadr909@gmail.com>
2026-07-30 23:45:12 +0300

    welcome message

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
- return response()->json([
-        'message' => 'test test test'
+    return response()->json([
+        'message' => 'Welcome to the MoonTech API',
+        'version' => '1.0.0',
     ]);
 });
